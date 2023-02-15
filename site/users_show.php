8eeb55b6712a2b100fe3ebc3028efaa0df6d657a
Mert Babur <mertbabur@hotmail.nl>
2023-02-15 10:37:22 +0100

    Details werkend

--- site/users_show.php
+++ site/users_show.php
@@ -1,14 +1,13 @@
 <?php
 require 'database.php';
 
-
+$id = $_GET['user_id'];
 $stmt = $conn->prepare("SELECT id, first_name, last_name, email, password, ip_address FROM users WHERE id = :id");
 $stmt->bindParam(':id', $id);
 $stmt->execute();
 
 // set the resulting array to associative
 $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-$all_users = $stmt->fetchAll();
-
+$user = $stmt->fetch();
 
 echo "Details van " . $user['first_name'];
